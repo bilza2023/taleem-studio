f2f90b708822ac7ed32e60416f8571b09c7b5163
bilza2023 <bilza2023@gmail.com>
2026-08-23 23:56:00 +0500

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-
-import { vitePreprocess }
-	from "@sveltejs/vite-plugin-svelte";
-
-import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-node';
 
 export default {
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: adapter({
-			fallback: "index.html"
-		})
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  }
 };
