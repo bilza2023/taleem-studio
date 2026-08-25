014adf1e34e67135d29ac929994e6a8ebbfaa5ad
bilza2023 <bilza2023@gmail.com>
2026-08-25 05:03:33 +0500

    hell

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,11 +1,11 @@
 ///home/bilal-tariq/00--TALEEM/taleem.studio/src/lib/config.js
 export const config = {
-  // apiUrl: "https://taleem.help/admin",
-  // imageDir : '/root/taleem-studio/content/images',
-  // contentDir: "/root/taleem-studio/content",
-  // basePath: "/admin",
-  apiUrl: "http://localhost:5173",
-  imageDir : '/home/bilal-tariq/00--TALEEM/taleem.studio/content/images',
-  contentDir: "/home/bilal-tariq/00--TALEEM/taleem.studio/content",
-  basePath: "",
+  apiUrl: "https://taleem.help/admin",
+  imageDir : '/root/taleem-studio/content/images',
+  contentDir: "/root/taleem-studio/content",
+  basePath: "/admin",
+  // apiUrl: "http://localhost:5173",
+  // imageDir : '/home/bilal-tariq/00--TALEEM/taleem.studio/content/images',
+  // contentDir: "/home/bilal-tariq/00--TALEEM/taleem.studio/content",
+  // basePath: "",
 };
